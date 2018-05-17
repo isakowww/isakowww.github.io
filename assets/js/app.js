317cf9b05db30cc8c58d90af38cc70825ae31aac
Nikola Isakov <nikolaisakov@gmail.com>
2018-05-17 17:17:00 +0200

    Simple landing page 'isakowww.com' completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,5 @@
 
-$('.main-tilt-container').tilt({
-	'transform-style': 'preserve-3d'
-});
-
-$('.logo').tilt({
-	'transform': 'translateZ(20px)'
-});
-
-$(document).ready(function(){
-    $("button").click(function(){
-        $(".contact-form").addClass("active");
-    });
-
-    $(".close-form").click(function(){
-        $(".contact-form").removeClass("active");
-    });
+$('.logo').universalTilt({
+	'position-base': 'window',
+	'max': 15
 });
